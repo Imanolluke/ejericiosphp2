bf503185494712619813dc072d361fb51e74d6e7
imanolluke <imanolluke@gmail.com>
2021-10-11 10:40:46 +0200

    3 terminado

--- 3.php
+++ 3.php
@@ -18,18 +18,37 @@ class Numeros {
 
     }   
 
-    function crearArray(){
-
+    function cargarNums(){
+        $menor = 100;
+        $mayor = 0;
+        $suma = 0;
         for($i=0; $i<20; $i++){
+            $numeros[$i] = rand(1, 100);
+            if($numeros[$i]<$menor){
+                $menor = $numeros[$i];
+            }
+            if($mayor<$numeros[$i]){
+                $mayor = $numeros[$i];
+            }
+            $suma = $suma+$numeros[$i];
+            number_format($suma, 2);
 
-            
+            echo $numeros[$i];
+            echo "<br>";
 
         }
+        $media = $suma/20;
+        echo "<p style = color:blue> el menor número es: $menor </p>";
+        echo "<p style = color:red> el mayor número es:  $mayor </p>";
+        echo "<p> la media de los números es:  $media </p>";
+        echo "<p>el total es:   $suma </p>";
 
-        return $numeros[];
 
     }
 }
+
+$n1 = new Numeros();
+$n1 -> cargarNums();
 
 
 
